8cf0856cc37de276270f8056ee8a5d3e9dd6a017
Duong Anh Tu <74184592+DuongTu263@users.noreply.github.com>
2023-08-31 01:30:21 +0700

    dung UI header

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import Tippy from "@tippyjs/react/headless";
+import "tippy.js/dist/tippy.css";
+
+import Button from "src/components/Button";
+import { Wrapper as PopperWrapper } from "src/components/Popper";
+import AccountItem from "src/components/AccountItem";
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
 
 function Header() {
-  return <h1>Header</h1>;
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 3000);
+  }, []);
+
+  return (
+    <header className={cx("wapper")}>
+      <div className={cx("content")}>
+        <div className={cx("logo")}>
+          <img src="src/assets/images/logo.svg" alt="Tiktok" />
+        </div>
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>Accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input placeholder="Search account and videos" spellCheck="false" />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+        <div className={cx("actions")}>
+          <Button light leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+            Tải lên
+          </Button>
+          <Button primary>Đăng nhập</Button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
